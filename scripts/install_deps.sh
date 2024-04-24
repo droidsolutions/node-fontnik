f372ef3f058927c87f390de15da62868c3337a8a
Anton Anders <anton.anders@droidsolutions.de>
2024-04-24 23:09:36 +0200

    fix: use mason again, wip, for now

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -9,11 +9,16 @@ function install() {
 }
 
 # setup mason
-#./scripts/setup.sh --config local.env
-#source local.env
+./scripts/setup.sh --config local.env
+source local.env
 
-# install boost 1.67.0
-# install freetype 2.7.1
-# install protozero 1.6.8
-# install sdf-glyph-foundry 0.2.0
-# install gzip-hpp 0.1.0
+install boost 1.67.0
+install freetype 2.7.1
+install protozero 1.6.8
+install sdf-glyph-foundry 0.2.0
+install gzip-hpp 0.1.0
+# pipx install conan
+#pipx ensurepath
+
+#pipx run conan profile detect || true
+#pipx run conan install conanfile.txt --build=missing
